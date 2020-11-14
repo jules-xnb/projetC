68ab90f15c9c56874d523d8bbc3b35d892f5e016
Jules Bertrand <jules.bertrand@etu.u-pec.fr>
2020-11-14 17:31:07 +0100

    ajout lecture des éléments plus ou moins statique exo1

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define TAILLE_MAX 100
 
 int verifierDoctype(FILE* f){
-    char* buffer = malloc(sizeof(char)*100);
+    char* buffer = malloc(sizeof(char)*TAILLE_MAX);
     fgets(buffer, 11, f);
     if (strcmp("<!DOCTYPE ",buffer)==0){
         printf("DOCTYPE CORRECT\n");
@@ -19,7 +20,7 @@ int verifierDoctype(FILE* f){
 }
 
 char* getNomDTD(FILE* f){
-    char* buffer = malloc(sizeof(char)*100);
+    char* buffer = malloc(sizeof(char)*TAILLE_MAX);
     int i = 0 ;
     char c = fgetc(f) ;
     int save = 0 ;
@@ -49,7 +50,114 @@ void sauterLigne(FILE* f){
     char c ;
     do {
         c = fgetc(f);
+        if(c != ' ' && c != '\n'){
+            printf("ERREUR : CARACTERE INNATENDU");
+            exit(1);
+        }
     } while (c != '\n') ;
+}
+
+int finDeDTD(FILE* f){
+    char c = fgetc(f);
+    if (c == ']'){
+        c = fgetc(f);
+        if (c == '>'){
+            printf("FIN DE DTD");
+            return 0 ;
+        } else {
+            printf("ERREUR : CARACTERE INNATENDU");
+            exit(1);
+            return 0 ;
+        }
+    } else {
+        fseek(f,-1,SEEK_CUR);
+        return 1 ;
+    }
+}
+
+void enleverEspaces(FILE* f){
+    char c = fgetc(f) ;
+    while (c == 9 || c == ' '){
+        c = fgetc(f) ;
+    }
+    fseek(f, -1, SEEK_CUR) ;
+}
+
+int detectionElement (FILE* f){
+    char* buffer = malloc(sizeof(char)*TAILLE_MAX);
+    enleverEspaces(f);
+    fgets(buffer, 11, f) ;
+    if (strcmp("<!ELEMENT ", buffer)==0){
+        printf("DETECTION D'UN ELEMENT\n") ;
+        free(buffer) ;
+        return 1 ;
+    } else {
+        free(buffer) ;
+        printf("ELEMENT INCORRECT\n") ;
+        return 0 ;
+    }
+
+}
+
+void getNameElement(int i, char** tabNameElement, FILE* f){
+    int j = 0 ;
+    char c = fgetc(f);
+    char* buffer = malloc(sizeof(char)*TAILLE_MAX);
+    while (c != ' '){
+        buffer[j] = c ;
+        //printf("|%c|",c );
+        c = fgetc(f);
+        j++ ;
+    }
+    tabNameElement[i] = buffer ;
+    printf("L'élément est : %s \n", tabNameElement[i]);
+
+}
+
+void getAttributElement (int i, char** tabAttributElement, FILE* f){
+    char* buffer = malloc(sizeof(char)*TAILLE_MAX);
+    int j = 0 ;
+    char c = fgetc(f);
+    while (c != ')'){
+        buffer[j] = c ;
+        c = fgetc(f);
+        j++ ;
+    }
+    tabAttributElement[i] = buffer ;
+    printf("L'attribut associé est : %s \n", tabAttributElement[i] );
+
+}
+
+int checkParenthese(FILE* f){
+    char c = fgetc(f);
+    if (c == '('){
+            return 1 ;
+        } else {
+            return 0 ;
+        }
+
+}
+
+void getElement(int i, char** tabNameElement, char** tabAttributElement, FILE* f){
+    if (detectionElement(f) == 1 ){
+        getNameElement(i,tabNameElement,f);
+        if (checkParenthese(f) == 1){
+            getAttributElement(i, tabAttributElement, f);
+            if (fgetc(f) == '>'){
+                sauterLigne(f);
+            } else {
+                printf("ERREUR DE FIN D'ELEMENT");
+                exit(1);
+            }
+        } else {
+            printf("ERREUR DE SYNTAXE DE L'ATTRIBUT DE L'ELEMENT");
+            exit(1);
+        }
+
+    } else {
+        exit(1) ;
+    }
+
 }
 
 int main()
@@ -66,8 +174,22 @@ int main()
     printf("Nom de la DTD : %s\n",nomDTD);
 
     sauterLigne(dtd);
-    char c = fgetc(dtd);
-    printf("%c",c);
+
+    char** tabNameElement = malloc(sizeof(char*)*TAILLE_MAX);
+    char** tabAttributElement = malloc(sizeof(char*)*TAILLE_MAX);
+    int i = 0 ;
+
+    while(finDeDTD(dtd) == 1 ){
+        getElement(i, tabNameElement, tabAttributElement, dtd) ;
+        i++ ;
+    }
+
+
+
+    // lireElement
+    // mettre en place une sécuritée au cas
+    // ou le caractere souhaité n'est
+    // pas présent dans les while (c ==
 
 
     fclose (dtd);
