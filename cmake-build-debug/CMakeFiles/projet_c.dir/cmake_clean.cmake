3b61900921ae162a4785c478821b582991368308
Maxime LALO <maxime.lalo.pro@gmail.com>
2020-11-25 20:00:39 +0100

    Clion added

---
+++ cmake-build-debug/CMakeFiles/projet_c.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projet_c.dir/main.c.obj"
+  "libprojet_c.dll.a"
+  "projet_c.exe"
+  "projet_c.exe.manifest"
+  "projet_c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/projet_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
